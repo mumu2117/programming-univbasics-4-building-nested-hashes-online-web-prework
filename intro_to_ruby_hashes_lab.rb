576538ba5f70744dc4bd86fd8208dfc19901d372
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-21 00:45:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,14 +6,8 @@ end
 
 def monopoly_with_second_tier
  
- monopoly = {
-		railroads: {
-			pieces: 4,
-			names: {},
-			rent_in_dollars: {}
-		}
-	}
-
+  {:railroads => {:pieces => 4}}
+  
 end
 
 def monopoly_with_third_tier
